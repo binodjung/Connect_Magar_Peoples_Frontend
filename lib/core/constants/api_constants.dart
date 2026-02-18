b8e9f0a4e276d4c9bb268e0bf501d1d2bcedc1e1
binodjung <bnodtha1122@gmail.com>
2026-02-18 11:34:45 +0545

    resolved like and comment issue

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,9 +2,8 @@ class ApiConstants {
   // Use 10.0.2.2 for Android Emulator
   // static const String baseUrl = 'http://10.0.2.2:8000/api'; 
   
-  // Use 192.168.1.7 for Physical Device (same Wi-Fi)
-  // Use 192.168.1.7 for Physical Device (same Wi-Fi)
-  static const String baseUrl = 'http://192.168.1.7:8000/api'; 
+  // Use 192.168.1.8 for Physical Device (same Wi-Fi)
+  static const String baseUrl = 'http://192.168.1.8:8000/api'; 
   
   static const String login = '$baseUrl/auth/login/';
   static const String register = '$baseUrl/auth/register/';
